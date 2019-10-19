f0e55e229c0c8581bbcf86b030331d6b00775584
Reyan <reyanrahman@gmail.com>
2019-10-19 13:56:39 +0530

    Updates

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+let hour = document.getElementById('hour'),
+    day = document.getElementById('day'),
+    min = document.getElementById('min'),
+    sec = document.getElementById('sec');
+function showTime(){  
+    let today = new Date();
+        hour.innerText=23-today.getHours();
+        min.innerText=59-today.getMinutes();
+        day.innerText= 24-today.getDate();
+        sec.innerText=60-today.getSeconds();
+        setTimeout(showTime,1000);
+}
+showTime();
+let o=1
+let nav=document.querySelector("nav");
+function open_nav(){
+    
+    if(o===1 && window.innerWidth<700){
+       nav.style.display="block"
+        o=0
+    }
+    else if(o===0 && window.innerWidth<700){
+        nav.style.display="none"
+        o=1
+    }
+}
+window.addEventListener("resize",()=>{nav.style.display="block";o=0});
